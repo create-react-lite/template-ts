f1b62632a6c31ee30fefa032dbca44a9c090f772
xheartean <info@gokhankoc.pl>
2021-01-31 11:15:10 +0100

    testing suite ready with typescript

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,15 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
+import Counter from "./Components/Counter/Counter";
 import "./index.css";
 
 const App = () => {
-  return <div>hey</div>;
+  return (
+    <>
+      <div>Your create-react-lite application is ready.</div>
+      <Counter />
+    </>
+  );
 };
 
 ReactDOM.render(<App />, document.querySelector("#root"));
--- webpack.dev.js
+++ webpack.dev.js
@@ -11,7 +11,7 @@ module.exports = {
     publicPath: "",
   },
   resolve: {
-    extensions: [".js", ".jsx", "ts", "tsx"],
+    extensions: [".js", ".jsx", ".ts", ".tsx"],
   },
   module: {
     rules: [
